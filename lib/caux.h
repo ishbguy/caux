f2bc63e2a2df08a75d69ba5c2e6b2931312d7c8a
Herbert Shen <ishbguy@hotmail.com>
2017-05-16 20:27:36 +0800

    Add caux.h to include all *.h files.

---
+++ lib/caux.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Herbert Shen <ishbguy@hotmail.com>
+ *               All Rights Reserved.
+ */
+
+#ifndef __CAUX_H__
+#define __CAUX_H__
+
+#include "xmacro.h"
+#include "smalloc.h"
+#include "scope.h"
+#include "eprintf.h"
+#include "erract.h"
+#include "logact.h"
+
+#endif /* End of include guard: __CAUX_H__ */
+
+/* vim:set ft=c ts=4 sw=4: */
