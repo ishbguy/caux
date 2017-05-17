a3d3570545b6cc8c495c30250eae4bbf2afcfa7f
Herbert Shen <ishbguy@hotmail.com>
2017-05-18 00:17:14 +0800

    Add test framework.

---
+++ test/framework.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 Herbert Shen <ishbguy@hotmail.com>
+ *               All Rights Reserved.
+ */
+
+#include "framework.h"
+
+int main_ret = 0;
+int test_count = 0;
+int test_pass = 0; 
+
+static void run_test(UnitTest test)
+{
+    test();
+}
+
+void run_tests(UnitTest *tests)
+{
+    for (int i = 0; tests[i] != NULL; i++)
+        run_test(tests[i]);
+}
+
+/* vim:set ft=c ts=4 sw=4: */
