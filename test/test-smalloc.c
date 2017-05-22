b5e47b57fcd2a5c94dc7ba38d45c37373e4d7066
Herbert Shen <ishbguy@hotmail.com>
2017-05-22 16:25:41 +0800

    Add smalloc test cases.

--- test/test-smalloc.c
+++ test/test-smalloc.c
@@ -4,18 +4,47 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
 #include "../lib/xmacro.h"
 #include "../lib/smalloc.h"
 #include "../lib/cunit.h"
 
 void test_smalloc(void)
 {
-    
+    void *ptr;
+
+    EXPECT_EQ_NULL(MALLOC(ptr, 0));
+    FREE(ptr);
+    EXPECT_NE_NULL(NEW(ptr));
+    FREE(ptr);
+    EXPECT_NE_NULL(MALLOC(ptr, UINT32_MAX));
+    FREE(ptr);
+    EXPECT_EQ_NULL(MALLOC(ptr, UINT32_MAX+1));
+    FREE(ptr);
+    EXPECT_NE_NULL(MALLOC(ptr, -1));
+    FREE(ptr);
+
+    EXPECT_EQ_NULL(CALLOC(ptr, 1, 0));
+    FREE(ptr);
+    EXPECT_EQ_NULL(CALLOC(ptr, 0, 0));
+    FREE(ptr);
+    EXPECT_EQ_NULL(CALLOC(ptr, 0, 1));
+    FREE(ptr);
+    EXPECT_NE_NULL(NEW0(ptr));
+    FREE(ptr);
+    EXPECT_NE_NULL(CALLOC(ptr, 1, UINT32_MAX));
+    FREE(ptr);
+    EXPECT_EQ_NULL(CALLOC(ptr, 1, UINT32_MAX+1));
+    FREE(ptr);
+    EXPECT_NE_NULL(CALLOC(ptr, 1, UINT32_MAX*2));
+    FREE(ptr);
+    EXPECT_NE_NULL(CALLOC(ptr, 1, -1));
+    FREE(ptr);
 }
 
 UnitTest tests[] = {
     test_smalloc,
-    NULL, /* array end indicator */
+    NULL,                       /* array end indicator */
 };
 
 int main(void)
