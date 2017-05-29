95bef15caf6cf253fe30d373aa6637d5c7c58b21
Herbert Shen <ishbguy@hotmail.com>
2017-05-29 15:19:46 +0800

    Update SCOPE_GUARD() macro.

--- lib/scope.h
+++ lib/scope.h
@@ -9,26 +9,26 @@
 #include "stack.h"
 #include "xmacro.h"
 
-/* 
+/*
  * Create a stack to store resource release functions.
  */
-#define SCOPE_INIT() Stack *scope = stack_new(2);            \
-                    if(scope == NULL) goto Clean;
+#define SCOPE_INIT() stack_t *__scope = stack_new(2)
 
-/* 
+/*
  * Add resource release functions to scope stack.
  */
-#define SCOPE_GUARD(callback)    stack_push(scope, callback)
+#define SCOPE_GUARD(callback) \
+    stack_push(__scope, LAMBDA(void, (void), {callback;}))
 
-/* 
+/*
  * Use resource release functions in the scope stack to recycle
  * resource, then free scope stack.
  */
-#define SCOPE_CLEAN()             do {                      \
-    for (void (*fn)(void) = stack_pop(scope);               \
-            fn != NULL; fn = stack_pop(scope))              \
-        fn();                                              \
-    stack_free(scope);                                      \
+#define SCOPE_CLEAN() do {                       \
+    for (void (*fn)(void) = stack_pop(__scope);  \
+            fn != NULL; fn = stack_pop(__scope)) \
+        fn();                                    \
+    stack_free(__scope);                         \
 } while(0)
 
 #endif /* End of include guard: __SCOPE_H__ */
